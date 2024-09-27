03b136dcffae220d93c3198b1acd3896588193d1
isaac neiff <isaacalves98017@gmail.com>
2024-09-27 10:23:02 -0300

    270920241021

---
+++ js/mem_game.js
@@ -0,0 +1,17 @@
+const input = document.querySelector('.login_input')
+const button = document.querySelector('.login_button')
+const form = document.querySelector('.login_form')
+
+const validateInput = ({ target }) => {
+    if (target.value.length > 4) {
+        button.removeAttribute('disabled')
+    } else {
+        button.setAttribute('disabled', '')
+    }
+}
+
+const handleSubmit = () =>
+
+
+    input.addEventListener('input', validateInput)
+form.addEventListener('submit', handleSubmit)
